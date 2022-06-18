a4c3226c491c370935082a3d3c220a7cccce96e0
Duncan kariuki <duncankariuki60@gmail.com>
2022-06-18 23:24:46 +0300

    initial commit

---
+++ src/main/kotlin/com/dKariuki/nftstore/NftstoreApplication.kt
@@ -0,0 +1,11 @@
+package com.dKariuki.nftstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NftstoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<NftstoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/dKariuki/nftstore/NftstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dKariuki.nftstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NftstoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
